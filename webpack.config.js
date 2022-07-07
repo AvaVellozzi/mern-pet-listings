89d25b5438ef32369afaeffeed02411fbcc50350
AvaVellozzi <arvello45@gmail.com>
2022-07-07 19:08:51 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const nodeExternals = require("webpack-node-externals")
+const path = require("path")
+
+const typicalReact = {
+  rules: [
+    {
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-react"]
+        }
+      }
+    }
+  ]
+}
+
+const clientConfig = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "main.js"
+  },
+  mode: "development",
+  module: typicalReact
+}
+
+const serverConfig = {
+  entry: "./server.js",
+  output: {
+    path: __dirname,
+    filename: "server-compiled.js"
+  },
+  externals: [nodeExternals()],
+  target: "node",
+  mode: "production",
+  module: typicalReact
+}
+
+module: {
+  rules: [
+    { test: /\.ts$/, loader: "ts-loader" },  
+    { test: /\.node$/, use: "node-loader"}
+  ]
+}
+
+module.exports = [clientConfig, serverConfig]
